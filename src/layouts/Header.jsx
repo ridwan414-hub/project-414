eab902815aeb630a8727e4c7e4bf28f076f6ad3a
yepsamii <abrahmansw@gmail.com>
2024-05-21 23:11:17 +0600

    Fix responsive width

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,76 +1,76 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    // <div>
-    //   <ul>
-    //     <li>
-    //       <Link to="/">Home</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/about-us">About us</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/donar-list">Donar List</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/committee">Committee</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/provincial-committee">Provincial Committee</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/gallery">Gallery</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/events">Events</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/blog">Blog</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/members">Members</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/contact-us">Contact Us</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/privacy-policy">Privacy Policy</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/donate">Donate</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/join">Join</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/login">Login</Link>
-    //     </li>
-    //   </ul>
-    // </div>
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="navbar bg-base-100 flex justify-center">
+      <div className="max-w-[1280px] w-full flex justify-between">
+        <div className="navbar-start w-fit">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about-us">About us</Link>
+              </li>
+              <li>
+                <Link to="/donar-list">Donar List</Link>
+              </li>
+              <li>
+                <Link to="/committee">Committee</Link>
+              </li>
+              <li>
+                <Link to="/provincial-committee">Provincial Committee</Link>
+              </li>
+              <li>
+                <Link to="/gallery">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/join">Join</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/committee">Committee</Link>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
             <li>
               <Link to="/">Home</Link>
             </li>
@@ -84,111 +84,34 @@ const Header = () => {
               <Link to="/committee">Committee</Link>
             </li>
             <li>
-              <Link to="/provincial-committee">Provincial Committee</Link>
+              <details>
+                <summary>Committee</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
             </li>
             <li>
-              <Link to="/gallery">Gallery</Link>
+              <Link to="/provincial-committee">Provincial Committee</Link>
             </li>
+
             <li>
               <Link to="/events">Events</Link>
             </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/members">Members</Link>
-            </li>
-            <li>
-              <Link to="/contact-us">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/privacy-policy">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link to="/donate">Donate</Link>
-            </li>
+
             <li>
               <Link to="/join">Join</Link>
             </li>
             <li>
               <Link to="/login">Login</Link>
             </li>
-            <li>
-              <Link to="/committee">Committee</Link>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about-us">About us</Link>
-          </li>
-          <li>
-            <Link to="/donar-list">Donar List</Link>
-          </li>
-          <li>
-            <Link to="/committee">Committee</Link>
-          </li>
-          <li>
-            <details>
-              <summary>Committee</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <Link to="/provincial-committee">Provincial Committee</Link>
-          </li>
-          <li>
-            <Link to="/gallery">Gallery</Link>
-          </li>
-          <li>
-            <Link to="/events">Events</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/members">Members</Link>
-          </li>
-          <li>
-            <Link to="/contact-us">Contact Us</Link>
-          </li>
-          <li>
-            <Link to="/privacy-policy">Privacy Policy</Link>
-          </li>
-          <li>
-            <Link to="/donate">Donate</Link>
-          </li>
-          <li>
-            <Link to="/join">Join</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
       </div>
     </div>
   );
