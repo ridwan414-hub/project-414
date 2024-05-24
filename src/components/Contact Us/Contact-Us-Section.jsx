083c04d3323c64fd21ff4ca66a618022dedb3082
yepsamii <abrahmansw@gmail.com>
2024-05-25 05:27:40 +0600

    chore: Update npm dependencies and add react-icons package

--- src/components/Contact Us/Contact-Us-Section.jsx
+++ src/components/Contact Us/Contact-Us-Section.jsx
@@ -1,11 +1,18 @@
 
+import ContactPart from "./Contact-Part";
+import FindLocationSection from "./Find-Location-Section";
 
 const ContactUsSection = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <ContactPart />
+      <section className="flex justify-center my-10">
+        <div className="w-full flex gap-7 max-w-7xl">
+          <FindLocationSection />
         </div>
-    );
+      </section>
+    </>
+  );
 };
 
 export default ContactUsSection;
---
+++ src/components/Contact Us/Find-Location-Section.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const FindLocationSection = () => {
+  return (
+    <div className="w-full flex flex-col gap-7">
+      <h2 className="font-bold text-lg pb-1 border-b-2 border-red-500">
+        Find Our Location
+      </h2>
+      <div style={{ width: "100%" }}>
+        <iframe
+          width="100%"
+          height={600}
+          src="https://maps.google.com/maps?width=100%25&height=600&hl=en&q=106/A,Babunagar,Feni%20,%20Bangladesh+(My%20Business%20Name)&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+        >
+          <Link to="https://www.gps.ie/">gps devices</Link>;
+        </iframe>
+      </div>
+    </div>
+  );
+};
+
+export default FindLocationSection;
