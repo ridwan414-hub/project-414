220deaad8443254c58fdbbf3acf6f54818cef0d3
yepsamii <abrahmansw@gmail.com>
2024-05-25 05:59:01 +0600

    chore: Add DiscussingSection component to Contact Us page

--- src/components/Contact Us/Contact-Us-Section.jsx
+++ src/components/Contact Us/Contact-Us-Section.jsx
@@ -1,5 +1,6 @@
 
 import ContactPart from "./Contact-Part";
+import DiscussingSection from "./Discussing-Section";
 import FindLocationSection from "./Find-Location-Section";
 
 const ContactUsSection = () => {
@@ -9,6 +10,7 @@ const ContactUsSection = () => {
       <section className="flex justify-center my-10">
         <div className="w-full flex gap-7 max-w-7xl">
           <FindLocationSection />
+          <DiscussingSection />
         </div>
       </section>
     </>
